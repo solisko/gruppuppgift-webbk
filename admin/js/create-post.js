68a29252781355971ecc401eaf1879723d060424
Sofia Skommar <sofiaskommar@gmail.com>
2023-12-06 21:41:55 +0100

    create js

--- admin/js/create-post.js
+++ admin/js/create-post.js
@@ -1,5 +1,26 @@
-document.getElementById('create-post-form').addEventListener('submit', createPost());
+document.getElementById('create-post-form').addEventListener('submit', createPost);
 
-async function createPost(){
+async function createPost(e) {
+    e.preventDefault();
+
+    let form = e.target;
     
+    try{
+        let formData = new FormData(form)
+        let data = {"content": formData.get('content')};
+
+        await fetch (`https://blog-api-assignment.up.railway.app/posts`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(data)
+            });
+
+            location.replace('index.html');
+
+
+    }catch(error){
+        console.log(`ERROR: ${error}`)
+    }
 }
