bec9c958cc79c508b0f856df4d6dd963ea49ecc2
Sofia Skommar <sofiaskommar@gmail.com>
2023-12-11 14:42:20 +0100

    read more done

---
+++ js/readmore.js
@@ -0,0 +1,22 @@
+console.log('Query string:',  location.search);
+const urlParams = new URLSearchParams(location.search)
+
+readPost();
+
+async function readPost() {
+    try {
+        const response = await fetch('https://blog-api-assignment.up.railway.app/posts/' + urlParams.get('id'));
+        const post = await response.json();
+        
+
+        document.getElementById('post-content').innerHTML = `
+        <h1>${post.title}</h1>
+        <i>${post.author} / ${new Date(post.date).toLocaleString()}</i>
+        <p>${post.content}</p>
+        <b>Tags:</b> <span> ${post.tags.join(', ')}</span>
+        `
+
+    } catch(error) {
+        console.log(error)
+    }  
+}
